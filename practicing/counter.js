d9f85717719653c697a2269cead1e9c17fa4d692
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2024-02-07 10:48:13 -0800

    js practicing

--- practicing/counter.js
+++ practicing/counter.js
@@ -1,16 +1,14 @@
-if (!localStorage.getItem('counter')) {
-  localStorage.setItem('counter',0);
-}
 
-function count() {
-  let counter = localStorage.getItem('counter');
-  counter++;
+let counter = 0;
+function count(){
+  counter++
   document.querySelector('h1').innerHTML = counter;
-  localStorage.setItem('counter',counter);
+  if(counter % 10 ===0) {
+    alert(`Count is now ${counter} `)
+  }
 }
+addEventListener('DOMContentLoaded', function(){
+  document.querySelector('button').onclick = count;
 
-document.addEventListener('DOMContentLoaded', function(){
-  document.querySelector('h1').innerHTML = localStorage.getItem('counter')
-  document.querySelector('button').addEventListener('click', count);
 
 })
