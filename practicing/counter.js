dc6c654c32a2790a3c7d690b9c5fb030eaf5a091
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2024-02-14 04:25:11 -0800

    i like commiting , its full of spirits

--- practicing/counter.js
+++ practicing/counter.js
@@ -1,13 +1,16 @@
-
-let counter = 0;
-function count(){
-  counter++
-  document.querySelector('h1').innerHTML = counter;
-  if(counter % 10 ===0) {
-    alert(`Count is now ${counter} `)
-  }
+if(!localStorage.getItem('counter')) {
+  localStorage.setItem('counter',0);
 }
-addEventListener('DOMContentLoaded', function(){
+
+
+function count(){
+  let counter = localStorage.getItem('counter');
+  counter++;
+  document.querySelector('h1').innerHTML = counter;
+  localStorage.setItem('counter',counter);  
+}
+document.addEventListener('DOMContentLoaded', function(){
+  document.querySelector('h1').innerHTML = localStorage.getItem('counter');
   document.querySelector('button').onclick = count;
 
 
